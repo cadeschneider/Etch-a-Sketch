86cdaf08f0324cf31c687ca02ec7b0308f774f68
Cade Schneider <cadeschneider25@gmail.com>
2022-11-08 10:57:26 -0800

    Added buttons, scroll bar, and improved overall customizability

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Etch-a-Sketch
+
+This is a browser version of a sketchpad and Etch-A-Sketch hybrid.
+
+ The purpose of this project is to show off my DOM manipulation skills I have developed over the last several months.
--- sketch.js
+++ sketch.js
@@ -1,34 +1,72 @@
-//Create pixel canvas
-const container = document.querySelector('.canvas');
-const pixel = document.createElement('div');
-
+//Function that sets pixel density
 function setDensity(size){
-    for (let i = 0; i < (size*size); i++) {
-        pixel.remove()
-   };
     for (let i = 0; i < (size*size); i++) {
         container.appendChild(pixel.cloneNode(true));
    };
 }
 
-setDensity(16)
+function getSprinkles(){
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
-//Listen for hovering mouse on pixels
-const pixels = document.querySelectorAll('div');
+//Function to set listen for hovering mouse on pixels
+function setListener(color="black"){
+    const pixels = document.querySelectorAll('div');
 
-pixels.forEach( item => item.addEventListener('mouseover', function(event){
+    if (color === "sprinkles"){
+        pixels.forEach( item => item.addEventListener('mouseover', function(event){
+            item.setAttribute('style',`background-color:${getSprinkles()};`);
+    } else{
 
-    item.setAttribute('style','background-color:blue;');
-}));
+        pixels.forEach( item => item.addEventListener('mouseover', function(event){
+            item.setAttribute('style',`background-color:${color};`);
+        }));
+
+    }};
+
+};
+
+//Function that initiates canvas size and adds listeners
+function setUp(size){
+    document.getElementById('canvas').innerHTML = ''
+    setDensity(size)
+    setListener()
+    container.setAttribute('style',`background-color:white; grid-template-columns: repeat(${size}, 1fr);`);
+}
+
+const container = document.querySelector('#canvas');
+const pixel = document.createElement('div');
+
+setUp(16)
+setListener();
 
 //Slider to adjust pixel density in canvas
 var density = document.getElementById("density");
+var output = document.getElementById("displaySize");
+output.textContent = `${density.value} x ${density.value}`;
 
 density.oninput = function() {
-
-    setDensity(16)
-    for (let i = 0; i < ((this.value*this.value) - (16*16)); i++) {
-        container.appendChild(pixel.cloneNode(true));
-    };
-    pixels.forEach( item => item.setAttribute('style',`background-color:red; width:${400/this.value}px; length:${400/this.value}px;`));
+    output.textContent = `${density.value} x ${density.value}`;
+    document.getElementById('canvas').innerHTML = ''
+    setUp(this.value);
 }
+
+const btn = document.querySelectorAll("button")
+btn.forEach( item => item.addEventListener('click', function(e){
+
+    if (e.target.id === "clearBtn"){
+        setUp(density.value)
+    }else if (e.target.id === "eraserBtn"){
+        setListener("white")
+    }else if (e.target.id === "colorBtn"){
+        setListener("red")
+    }else if (e.target.id === "sprinklesBtn"){
+        setListener("sprinkles")
+    }
+
+}));
