674dd3d517c35032ac61f612445d80ebf6301ef5
Cade Schneider <cadeschneider25@gmail.com>
2022-11-09 08:44:14 -0800

    Resolved syntax error on js

--- sketch.js
+++ sketch.js
@@ -1,72 +1,76 @@
-//Function that sets pixel density
-function setDensity(size){
-    for (let i = 0; i < (size*size); i++) {
-        container.appendChild(pixel.cloneNode(true));
-   };
-}
+document.addEventListener("DOMContentLoaded", function(event) {
+    
+    //Function that sets pixel density
+    function setDensity(size){
+        for (let i = 0; i < (size*size); i++) {
+            container.appendChild(pixel.cloneNode(true));
+    }}
 
-function getSprinkles(){
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+    function getSprinkles(){
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+        }
+        console.log(color);
+        return color;
+        
     }
-    return color;
-}
 
-//Function to set listen for hovering mouse on pixels
-function setListener(color="black"){
-    const pixels = document.querySelectorAll('div');
+    //Function to set listen for hovering mouse on pixels
+    function setListener(color="black"){
+        if (color === "sprinkles") {
+            const pixels = document.querySelectorAll('div');
+            pixels.forEach( item => item.addEventListener('mouseover', function(event){
+                item.setAttribute('style',`background-color:${getSprinkles()};`);
+            }));
+        } else {
+            const pixels = document.querySelectorAll('div');
+            pixels.forEach( item => item.addEventListener('mouseover', function(event){
+                item.setAttribute('style',`background-color:${color};`);
+            }));
 
-    if (color === "sprinkles"){
-        pixels.forEach( item => item.addEventListener('mouseover', function(event){
-            item.setAttribute('style',`background-color:${getSprinkles()};`);
-    } else{
+        }
+    }
 
-        pixels.forEach( item => item.addEventListener('mouseover', function(event){
-            item.setAttribute('style',`background-color:${color};`);
-        }));
+    //Function that initiates canvas size and adds listeners
+    function setUp(size){
+        document.getElementById('canvas').innerHTML = '';
+        setDensity(size);
+        setListener();
+        container.setAttribute('style',`background-color:white; grid-template-columns: repeat(${size}, 1fr);`);
+    }
 
-    }};
+    const container = document.querySelector('#canvas');
+    const pixel = document.createElement('div');
 
-};
+    setUp(16);
+    setListener();
 
-//Function that initiates canvas size and adds listeners
-function setUp(size){
-    document.getElementById('canvas').innerHTML = ''
-    setDensity(size)
-    setListener()
-    container.setAttribute('style',`background-color:white; grid-template-columns: repeat(${size}, 1fr);`);
-}
-
-const container = document.querySelector('#canvas');
-const pixel = document.createElement('div');
-
-setUp(16)
-setListener();
-
-//Slider to adjust pixel density in canvas
-var density = document.getElementById("density");
-var output = document.getElementById("displaySize");
-output.textContent = `${density.value} x ${density.value}`;
-
-density.oninput = function() {
+    //Slider to adjust pixel density in canvas
+    var density = document.getElementById("density");
+    var output = document.getElementById("displaySize");
     output.textContent = `${density.value} x ${density.value}`;
-    document.getElementById('canvas').innerHTML = ''
-    setUp(this.value);
-}
 
-const btn = document.querySelectorAll("button")
-btn.forEach( item => item.addEventListener('click', function(e){
+    density.oninput = function() {
+        output.textContent = `${density.value} x ${density.value}`;
+        document.getElementById('canvas').innerHTML = '';
+        setUp(this.value);
+    };
 
-    if (e.target.id === "clearBtn"){
-        setUp(density.value)
-    }else if (e.target.id === "eraserBtn"){
-        setListener("white")
-    }else if (e.target.id === "colorBtn"){
-        setListener("red")
-    }else if (e.target.id === "sprinklesBtn"){
-        setListener("sprinkles")
-    }
+    const btn = document.querySelectorAll("button");
+    btn.forEach( item => item.addEventListener('click', function(e){
 
-}));
+        if (e.target.id === "clearBtn"){
+            setUp(density.value);
+        }else if (e.target.id === "eraserBtn"){
+            setListener("white");
+        }else if (e.target.id === "colorBtn"){
+            setListener("red");
+        }else if (e.target.id === "sprinklesBtn"){
+            setListener("sprinkles");
+        }
+
+    }));
+
+});
